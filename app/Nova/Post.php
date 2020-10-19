1694bf74d781a7f08ddea82d91b3a9d97b9f15d0
Mior Muhammad Zaki <crynobone@gmail.com>
2020-10-19 13:09:34 +0800

    Add reproducing code for laravel/nova-issues#1901
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ app/Nova/Post.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Badge;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Trix;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Post extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\Post::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+            Text::make('Title', 'title')->sortable(),
+            Trix::make('Content', 'content')->nullable(),
+
+            Badge::make('Content (Display Using)', 'content')
+                ->displayUsing(function ($agreement) {
+                        return empty($agreement) ? 'danger' : 'success';
+                })
+                ->labels([
+                         'danger' => 'Missing',
+                        'success' => 'On File',
+                ])->sortable()->onlyOnIndex(),
+
+            Badge::make('Content (Resolve Using)', 'content')
+                ->resolveUsing(function ($agreement) {
+                        return empty($agreement) ? 'danger' : 'success';
+                })
+                ->labels([
+                    'danger' => 'Missing',
+                    'success' => 'On File',
+                ])->sortable()->onlyOnIndex(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
